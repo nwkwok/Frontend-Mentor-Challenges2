468d3b66fbc44a7d4a70dd0c94cceba4d5b43bc5
nwkwok <nwkwok@gmail.com>
2020-11-22 10:35:32 -0800

    get job card to render contact dynamically

--- static-job-listings-master/dist/index.dev.js
+++ static-job-listings-master/dist/index.dev.js
@@ -1,4 +1,11 @@
 "use strict";
 
-console.log(data);
-console.log(data[0].company);
+var jobCardArea = document.getElementById('jobCard');
+var languages = document.querySelectorAll('languages');
+data.forEach(function (data) {
+  jobCardArea.innerHTML += "\n    <div class=\"job-card-container\">\n    <div class=\"job-card-info\">\n      <div class=\"job-card-logo\">\n        <img id=\"logo\" src=".concat(data.logo, ">\n      </div>\n      <div class=\"job-card-company\">\n        <span id=\"company\">").concat(data.company, "</span>\n        <span id=\"new\">New!</span>\n        <span id=\"featured\">FEATURED</span>\n        <div class=\"job-card-title\">\n          <div id=\"jobTitle\">").concat(data.position, "</div>\n        </div>\n        <div class=\"job-card-details\">\n          <span id=\"postedAt\">").concat(data.postedAt, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"contact\">").concat(data.contract, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"location\">").concat(data.location, "</span>\n        </div>\n      </div>\n      <div class=\"job-card-filters\">\n        <span id=\"role\">").concat(data.role, "</span>\n        <span id=\"level\">").concat(data.level, "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[0], "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[1], "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[2], "</span>\n      </div>\n    </div>\n  </div>\n  ");
+
+  if (languages.textContent === 'undefined') {
+    languages.innerHTML === '<div></div>';
+  }
+});
--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -1,2 +1,42 @@
-console.log(data);
-console.log(data[0].company);
+const jobCardArea = document.getElementById('jobCard')
+const languages = document.querySelectorAll('languages')
+
+data.forEach((data) => {
+    jobCardArea.innerHTML += `
+    <div class="job-card-container">
+    <div class="job-card-info">
+      <div class="job-card-logo">
+        <img id="logo" src=${data.logo}>
+      </div>
+      <div class="job-card-company">
+        <span id="company">${data.company}</span>
+        <span id="new">New!</span>
+        <span id="featured">FEATURED</span>
+        <div class="job-card-title">
+          <div id="jobTitle">${data.position}</div>
+        </div>
+        <div class="job-card-details">
+          <span id="postedAt">${data.postedAt}</span>
+          <div class="divider"></div>
+          <span id="contact">${data.contract}</span>
+          <div class="divider"></div>
+          <span id="location">${data.location}</span>
+        </div>
+      </div>
+      <div class="job-card-filters">
+        <span id="role">${data.role}</span>
+        <span id="level">${data.level}</span>
+        <span class="languages" id="languages">${data.languages[0]}</span>
+        <span class="languages" id="languages">${data.languages[1]}</span>
+        <span class="languages" id="languages">${data.languages[2]}</span>
+      </div>
+    </div>
+  </div>
+  `
+  if (languages.textContent === 'undefined') {
+    languages.innerHTML === '<div></div>'
+}
+
+    
+});
+
