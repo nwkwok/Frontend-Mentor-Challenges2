53d8e59de97f42570e9a90b71e07b08a7569f992
nwkwok <nwkwok@gmail.com>
2020-11-22 15:12:57 -0800

    add filters and clear filters

--- static-job-listings-master/dist/index.dev.js
+++ static-job-listings-master/dist/index.dev.js
@@ -2,10 +2,40 @@
 
 var jobCardArea = document.getElementById('jobCard');
 var languages = document.querySelectorAll('languages');
+var jobFilters = document.getElementsByClassName('job-card-filters');
+var selectedFilters = document.getElementById('selectedFilters');
+var clearAllFilters = document.getElementById('clear');
+var deleteFilter = document.getElementsByClassName('remove');
+var prevSibling = deleteFilter.prevSibling;
+var filterArr = [];
 data.forEach(function (data) {
-  jobCardArea.innerHTML += "\n    <div class=\"job-card-container\">\n    <div class=\"job-card-info\">\n      <div class=\"job-card-logo\">\n        <img id=\"logo\" src=".concat(data.logo, ">\n      </div>\n      <div class=\"job-card-company\">\n        <span id=\"company\">").concat(data.company, "</span>\n        <span id=\"new\">New!</span>\n        <span id=\"featured\">FEATURED</span>\n        <div class=\"job-card-title\">\n          <div id=\"jobTitle\">").concat(data.position, "</div>\n        </div>\n        <div class=\"job-card-details\">\n          <span id=\"postedAt\">").concat(data.postedAt, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"contact\">").concat(data.contract, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"location\">").concat(data.location, "</span>\n        </div>\n      </div>\n      <div class=\"job-card-filters\">\n        <span id=\"role\">").concat(data.role, "</span>\n        <span id=\"level\">").concat(data.level, "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[0], "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[1], "</span>\n        <span class=\"languages\" id=\"languages\">").concat(data.languages[2], "</span>\n      </div>\n    </div>\n  </div>\n  ");
-
-  if (languages.textContent === 'undefined') {
-    languages.innerHTML === '<div></div>';
-  }
+  jobCardArea.innerHTML += "\n    <div class=\"job-card-container\">\n    <div class=\"job-card-info\">\n      <div class=\"job-card-logo\">\n        <img id=\"logo\" alt=\"".concat(data.company, "-logo\" src=").concat(data.logo, ">\n      </div>\n      <div class=\"job-card-company\">\n        <span id=\"company\">").concat(data.company, "</span>\n        <span id=\"new\">New!</span>\n        <span id=\"featured\">FEATURED</span>\n        <div class=\"job-card-title\">\n          <div id=\"jobTitle\">").concat(data.position, "</div>\n        </div>\n        <div class=\"job-card-details\">\n          <span id=\"postedAt\">").concat(data.postedAt, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"contact\">").concat(data.contract, "</span>\n          <div class=\"divider\"></div>\n          <span id=\"location\">").concat(data.location, "</span>\n        </div>\n      </div>\n      <div class=\"job-card-filters\">\n        <span id=\"role\">").concat(data.role, "</span>\n        <span id=\"level\">").concat(data.level, "</span>\n        ").concat(data.languages.map(function (language) {
+    return "<span class=\"languages\">".concat(language, "</span>");
+  }).join(''), "\n      </div>\n    </div>\n  </div>\n  ");
 });
+
+for (var i = 0; i < jobFilters.length; i++) {
+  jobFilters[i].addEventListener('click', function (e) {
+    var filter = e.target.innerText;
+    filterArr.push(filter);
+    selectedFilters.innerHTML += "\n        <span>".concat(e.target.innerText, "</span>\n        <img id=\"remove\" class=\"remove\" src=\"images/icon-remove.svg\"></img>");
+
+    for (var _i = 0; _i < deleteFilter.length; _i++) {
+      deleteFilter[_i].addEventListener('click', function () {
+        prevSibling.innerHTML = '';
+      });
+    }
+
+    console.log(filterArr);
+  });
+}
+
+clearAllFilters.addEventListener('click', function () {
+  selectedFilters.innerHTML = '';
+}); //     if (filterArr.includes(e.target.innerText) === false) {
+//         const filter = e.target.innerText;
+//         filterArr.push(filter);
+//         [...new Set(filterArr)];
+//         console.log(...filterArr);
+//     }
+// })}
--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -1,13 +1,22 @@
 const jobCardArea = document.getElementById('jobCard')
 const languages = document.querySelectorAll('languages')
+const jobFilters = document.getElementsByClassName('job-card-filters');
+const selectedFilters = document.getElementById('selectedFilters');
+const clearAllFilters = document.getElementById('clear');
+
+const deleteFilter = document.getElementsByClassName('remove');
+const prevSibling = deleteFilter.prevSibling;
+
+
+const filterArr = [];
+
 
 data.forEach((data) => {
-
     jobCardArea.innerHTML += `
     <div class="job-card-container">
     <div class="job-card-info">
       <div class="job-card-logo">
-        <img id="logo" src=${data.logo}>
+        <img id="logo" alt="${data.company}-logo" src=${data.logo}>
       </div>
       <div class="job-card-company">
         <span id="company">${data.company}</span>
@@ -28,13 +37,44 @@ data.forEach((data) => {
         <span id="role">${data.role}</span>
         <span id="level">${data.level}</span>
         ${data.languages.map((language) => {
-
             return `<span class="languages">${language}</span>`
         }).join('')}
       </div>
     </div>
   </div>
   `
-
-  console.log(data.languages);
 });
+
+for (let i = 0; i < jobFilters.length; i++) {
+    jobFilters[i].addEventListener('click', (e) => {
+        const filter = e.target.innerText;
+        filterArr.push(filter);
+
+        selectedFilters.innerHTML += `
+        <span>${e.target.innerText}</span>
+        <img id="remove" class="remove" src="images/icon-remove.svg"></img>` 
+
+        for (let i = 0; i < deleteFilter.length; i++) {
+            deleteFilter[i].addEventListener('click', () => {
+                prevSibling.innerHTML='';
+                })
+            }
+
+        console.log(filterArr);
+    })}
+
+clearAllFilters.addEventListener('click', () => {
+    selectedFilters.innerHTML = ''
+});
+
+
+
+    //     if (filterArr.includes(e.target.innerText) === false) {
+    //         const filter = e.target.innerText;
+    //         filterArr.push(filter);
+    //         [...new Set(filterArr)];
+    //         console.log(...filterArr);
+    //     }
+    // })}
+
+
