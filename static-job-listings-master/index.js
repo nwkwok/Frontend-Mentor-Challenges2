ef598f4b3f2769896386144f9533d75d8adaa393
nwkwok <nwkwok@gmail.com>
2020-11-22 23:09:03 -0800

    get filter box to add and remove items to array

--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -5,10 +5,9 @@ const selectedFilters = document.getElementById('selectedFilters');
 const clearAllFilters = document.getElementById('clear');
 
 const deleteFilter = document.getElementsByClassName('remove');
-const prevSibling = deleteFilter.prevSibling;
+const previousSibling = deleteFilter.previousElementSibling
 
-
-const filterArr = [];
+let filterArr = [];
 
 
 data.forEach((data) => {
@@ -20,8 +19,8 @@ data.forEach((data) => {
       </div>
       <div class="job-card-company">
         <span id="company">${data.company}</span>
-        <span id="new">New!</span>
-        <span id="featured">FEATURED</span>
+        ${data.new ? '<span id="new">New!</span>' : '<span></span>'}
+        ${data.featured ? '<span id="featured">FEATURED</span>' : '<span></span>'}
         <div class="job-card-title">
           <div id="jobTitle">${data.position}</div>
         </div>
@@ -45,26 +44,42 @@ data.forEach((data) => {
   `
 });
 
+//Click on job filter and add it to the filters box
 for (let i = 0; i < jobFilters.length; i++) {
     jobFilters[i].addEventListener('click', (e) => {
+        e.stopPropagation();
         const filter = e.target.innerText;
         filterArr.push(filter);
 
         selectedFilters.innerHTML += `
         <span>${e.target.innerText}</span>
         <img id="remove" class="remove" src="images/icon-remove.svg"></img>` 
+        console.log(filterArr);
 
+        //Click on selected filter to delete
         for (let i = 0; i < deleteFilter.length; i++) {
             deleteFilter[i].addEventListener('click', () => {
-                prevSibling.innerHTML='';
-                })
-            }
+                const item = (deleteFilter[i].previousElementSibling.innerText);
+                if (filterArr.includes(item)) {
+                    filterArr.splice(filterArr.indexOf(item));
+                }
 
-        console.log(filterArr);
+                deleteFilter[i].previousElementSibling.innerText = '';
+                deleteFilter[i].parentNode.removeChild(deleteFilter[i]);
+                console.log(filterArr);
+
+            })
+        }
+
     })}
+
+
+
+
 
 clearAllFilters.addEventListener('click', () => {
     selectedFilters.innerHTML = ''
+    filterArr = [];
 });
 
 
