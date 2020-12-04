ff0326c0ebc0dae46c74b8f91fbe515d78088aa5
nwkwok <nwkwok@gmail.com>
2020-12-03 21:20:47 -0800

    change from img bkgrnd to img element

--- rest-countries-api-with-color-theme-switcher-master/src/components/Country/index.js
+++ rest-countries-api-with-color-theme-switcher-master/src/components/Country/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import styles from './Country.module.scss';
 
 function Country(props) {
-  // console.log(props);
   return (
       <div className={styles.card}>
-        <div className={styles.imageContainer} 
-          style={{
-            backgroundImage: `url(${props.flag})`,
-            }}>
+        <div className={styles.imageContainer}
+          // style={{
+          //   backgroundImage: `url(${props.flag})`,
+          //   }}>
+          >
+          <img className={styles.flag} src={props.flag} alt="flag"></img>
+
         </div>
         <h4>{props.country}</h4>
         <div className={styles.info}>
       
         <span className={styles.stats} >Population: </span> 
-        {props.population}<br />
+        {numberWithCommas(props.population)}<br />
         <span className={styles.stats} >Region: </span> 
         {props.region}<br />
         <span className={styles.stats} >Capital: </span> 
@@ -22,6 +24,10 @@ function Country(props) {
         </div>
       </div>
   );
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default Country;
