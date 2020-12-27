df9cab50c6da67cba1075533d9842fd2ca0426bc
nwkwok <nwkwok@gmail.com>
2020-12-27 12:28:05 -0800

    create responsive navbar

--- designo-multi-page-website/client/src/components/Button/Button.js
+++ designo-multi-page-website/client/src/components/Button/Button.js
@@ -3,8 +3,6 @@ import style from './Button.module.scss'
 
 function LightButton(props) {
 
-    console.log(props.size);
-
     return (
         <div className={
             `${style.buttonContainer} ${props.btn === 'light' ? style.light : style.dark} ${style[props.size]}`
--- designo-multi-page-website/client/src/components/Header/Header.js
+++ designo-multi-page-website/client/src/components/Header/Header.js
@@ -1,25 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DarkLogo from '../../assets/shared/desktop/logo-dark.png';
 import style from './Header.module.scss';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import hamburger from '../../assets/shared/mobile/icon-hamburger.svg';
+import close from '../../assets/shared/mobile/icon-close.svg';
 
 function Header() {
-  // const [mobile, setMobile] = useState(window.innerWidth < 400);
-  // console.log(mobile);
-  
+  const [isMobile, setMobile] = useState(window.innerWidth < 770);
+  // const [navBar, setNavBar] = useState(true)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const updateMedia = () => {
+    if (window.innerWidth < 770) {
+      setMobile(true);
+      // setNavBar(true)
+    } else if (window.innerWidth >= 770) {
+      setMobile(false);
+      // setNavBar(false)
+    }
+  };
+
+  useEffect(() => {
+    updateMedia();
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  });
+
+  console.log(isMobile);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const navMenu = isMobile ? (
+    <div onClick={handleClick} className={style.mobileNavIcon}>
+      <img
+        className={style.navType}
+        src={isOpen ? close : hamburger}
+        alt='nav-type'
+      />
+      <div style={isOpen ? {display: 'block'} : {display: 'none'}} className={style.mobileNavContainer}>
+        <ul className={style.mobileNavLinks}>
+          <li>
+            <Link to='/our-company'>OUR COMPANY</Link>
+          </li>
+          <li>
+            <Link to='/locations'>LOCATIONS</Link>
+          </li>
+          <li>
+            <Link to='contact'>CONTACT</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <div className={style.navContainer}>
+      <ul className={style.navLinks}>
+        <li>
+          <Link to='/our-company'>OUR COMPANY</Link>
+        </li>
+        <li>
+          <Link to='/locations'>LOCATIONS</Link>
+        </li>
+        <li>
+          <Link to='contact'>CONTACT</Link>
+        </li>
+      </ul>
+    </div>
+  );
 
   return (
     <div className={style.headerContainer}>
       <div className={style.logoContainer}>
-        <Link to='/'><img src={DarkLogo} alt='logo' /></Link>
+        <Link to='/'>
+          <img src={DarkLogo} alt='logo' />
+        </Link>
       </div>
-      <div className={style.navContainer}>
+      {navMenu}
+      {/* <div className={style.navContainer}>
         <ul className={style.navLinks}>
           <li><Link to='/our-company'>OUR COMPANY</Link></li>
           <li><Link to='/locations'>LOCATIONS</Link></li>
           <li><Link to='contact'>CONTACT</Link></li>          
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 }
